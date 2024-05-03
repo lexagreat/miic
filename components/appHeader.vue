2b82eb664bafda965aa1e5593c0a4b8049ba56bd
zorion <zorion1234@mail.ru>
2024-05-03 22:48:25 +0300

    adapt header

--- components/appHeader.vue
+++ components/appHeader.vue
@@ -7,25 +7,34 @@
                   <img src="@/assets/img/icons/logo.svg" alt="" />
                </nuxt-link>
             </div>
-            <ul class="header__list">
-               <li>
-                  <nuxt-link to="/" class="header__link header__sublink"
-                     ><span>Услуги</span>
-                     <svg
-                        width="8"
-                        height="6"
-                        viewBox="0 0 8 6"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
+            <div class="header__menu" :class="{ active: isMenuOpen }">
+               <ul class="header__list">
+                  <li :class="{ active: isSubmenuOpen && windowWidth <= 992 }">
+                     <button
+                        class="header__link header__sublink"
+                        @click.prevent="openSubmenu"
                      >
-                        <path
-                           d="M0.666667 4.66663L4 1.33329L7.33333 4.66663"
-                           stroke="#6A6A6A"
-                           stroke-linecap="round"
-                           stroke-linejoin="round"
-                        />
-                     </svg>
-                     <div class="header__spoiler">
+                        <span>Услуги</span>
+                        <svg
+                           width="8"
+                           height="6"
+                           viewBox="0 0 8 6"
+                           fill="none"
+                           xmlns="http://www.w3.org/2000/svg"
+                        >
+                           <path
+                              d="M0.666667 4.66663L4 1.33329L7.33333 4.66663"
+                              stroke="#6A6A6A"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                           />
+                        </svg>
+                     </button>
+                     <div
+                        class="header__spoiler"
+                        :class="{ collapse: windowWidth <= 992 }"
+                        ref="spoiler"
+                     >
                         <ul>
                            <li>
                               <nuxt-link to="/">
@@ -72,23 +81,62 @@
                            </li>
                         </ul>
                      </div>
-                  </nuxt-link>
-               </li>
-               <li>
-                  <nuxt-link to="/" class="header__link"
-                     ><span>Цены</span></nuxt-link
-                  >
-               </li>
-               <li>
-                  <nuxt-link to="/" class="header__link"
-                     ><span>О нас</span></nuxt-link
-                  >
-               </li>
-            </ul>
+                  </li>
+                  <li>
+                     <nuxt-link to="/" class="header__link"
+                        ><span>Цены</span></nuxt-link
+                     >
+                  </li>
+                  <li>
+                     <nuxt-link to="/" class="header__link"
+                        ><span>О нас</span></nuxt-link
+                     >
+                  </li>
+               </ul>
+            </div>
             <button class="header__order btn yellow">Заказать звонок</button>
+            <button
+               class="header__burger"
+               @click="headerWork"
+               :class="{ active: isMenuOpen }"
+            >
+               <span></span>
+            </button>
          </div>
       </div>
    </header>
 </template>
-<script setup></script>
+<script setup>
+import { slideToggle } from "#imports";
+import { ref, onMounted, onUnmounted } from "vue";
+
+let isSubmenuOpen = ref(false);
+let isMenuOpen = ref(false);
+let spoiler = ref(null);
+
+let windowWidth = ref(0);
+
+function headerWork() {
+   isMenuOpen.value = !isMenuOpen.value;
+}
+
+function handleResize() {
+   windowWidth.value = window?.innerWidth;
+}
+function openSubmenu() {
+   if (window.innerWidth > 992) {
+      return;
+   }
+   isSubmenuOpen.value = !isSubmenuOpen.value;
+   slideToggle(spoiler.value);
+}
+
+onMounted(() => {
+   handleResize();
+   window.addEventListener("resize", handleResize);
+});
+onUnmounted(() => {
+   window.removeEventListener("resize", handleResize);
+});
+</script>
 <style lang=""></style>
